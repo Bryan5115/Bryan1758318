01446356c9fd95e5fed46e4c4c96a9ef092d4adc
Bryan5115 <72111426+Bryan5115@users.noreply.github.com>
2023-09-10 14:43:38 -0600

    Add files via upload

---
+++ clase0509/app.js
@@ -0,0 +1,25 @@
+
+let x=10;
+let y=2;
+let resultado = x+y
+console.log(resultado)
+
+
+//forma de concatenar
+var uno= "hola";
+var dos= "mundo";
+var joined = uno + dos;
+console.log(joined)
+
+var numeroUno=200;
+var numeroDos=100;
+console.log(numeroUno+numeroDos)
+
+//
+
+prompt("Ingresa tu nombre");
+
+var nombre = prompt("ingresa tu nombre: ");
+console.log(nombre);
+//typeof dato que se almacena
+console.log(typeof nombre);
---
+++ clase0509/app2.js
@@ -0,0 +1,20 @@
+let numero1 = prompt("ingresa un numero");
+let numero2 = prompt("ingresa un segundo numero");
+
+//let resultado = numero1+numero2;
+
+
+//sumna
+let resultado= parseInt(numero1)+parseInt(numero2);
+console.log("el resultado de la suma es.. " + resultado)
+
+//resta
+
+let resultado1= parseInt(numero1)-parseInt(numero2);
+console.log("el resultado de la resta es.. " + resultado1)
+
+
+//multiplicacion
+
+let resultado2= parseInt(numero1)*parseInt(numero2);
+console.log("el resultado de la multi es.. " + resultado2)
